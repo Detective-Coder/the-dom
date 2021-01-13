0cbbb0df6517088b6ebf0382f30699e526bf30dd
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-13 12:08:28 -0500

    added the movieposter project

--- scripts/main.js
+++ scripts/main.js
@@ -58,3 +58,71 @@ function listToppings() {
 }
 
 listToppings();
+
+let movieSchedule = [
+  {
+    title: "Ralph Breaks the Internet",
+    rating: "PG",
+    currentlyPlaying: true,
+    poster: "https://lumiere-a.akamaihd.net/v1/images/r_ralphbreakstheinternet_header_ddt-17403_08ef6d92.jpeg?region=0,0,1024,1274&optimize=true"
+  },
+  {
+    title: "The Grinch",
+    rating: "G",
+    currentlyPlaying: true,
+    poster: "https://images-na.ssl-images-amazon.com/images/I/71vgX4VFdiL._SY679_.jpg"
+  },
+  {
+    title: "A Star is Born",
+    rating: "R",
+    currentlyPlaying: false,
+    poster: "https://images-na.ssl-images-amazon.com/images/I/718zWbDKmvL._SY606_.jpg"
+  },
+   {
+    title: "Bohemian Rhapsody",
+    rating: "PG-13",
+    currentlyPlaying: true,
+    poster: "https://www.snhrc.com/wp-content/uploads/2018/09/Image-Coming-Soon.png"
+  },
+  {
+    title: "Fantastic Beasts: The Crimes of Grindlewald",
+    rating: "PG-13",
+    currentlyPlaying: true,
+    poster: "https://images-na.ssl-images-amazon.com/images/I/716fk%2BBZ-NL._SY606_.jpg"
+  },
+  {
+    title: "Robin Hood",
+    rating: "PG-13",
+    currentlyPlaying: false,
+    poster: "https://images-na.ssl-images-amazon.com/images/I/61K%2BTyGjsCL.jpg"
+  },
+  {
+    title: "Spider-Man: Into the Spider-Verse",
+    rating: "PG-13",
+    currentlyPlaying: true,
+    poster: "https://www.snhrc.com/wp-content/uploads/2018/09/Image-Coming-Soon.png"
+  }
+]
+
+// Use a for loop to loop through the movies, build an HTML representation of each movie, and insert it into the #movie-schedule container in the DOM.
+for (let i = 0; i < movieSchedule.length; i++) {
+  if (movieSchedule[i].currentlyPlaying && movieSchedule[i].rating === "G") {
+    document.querySelector("#movie-schedule").innerHTML += `<div class="green-border movie-card"><h2>${movieSchedule[i].title}</h2> <h3>${movieSchedule[i].rating}</h3> <h3>Showings today? ${movieSchedule[i].currentlyPlaying}</h3> <img src="${movieSchedule[i].poster}"></div>`;
+  } else if (movieSchedule[i].currentlyPlaying && movieSchedule[i].rating === "PG") {
+    document.querySelector("#movie-schedule").innerHTML += `<div class="yellow-border movie-card"><h2>${movieSchedule[i].title}</h2> <h3>${movieSchedule[i].rating}</h3> <h3>Showings today? ${movieSchedule[i].currentlyPlaying}</h3> <img src="${movieSchedule[i].poster}"></div>`;
+  } else if (movieSchedule[i].currentlyPlaying && movieSchedule[i].rating === "PG-13") {
+    document.querySelector("#movie-schedule").innerHTML += `<div class="orange-border movie-card"><h2>${movieSchedule[i].title}</h2> <h3>${movieSchedule[i].rating}</h3> <h3>Showings today? ${movieSchedule[i].currentlyPlaying}</h3> <img src="${movieSchedule[i].poster}"></div>`;
+  } else {
+    document.querySelector("#movie-schedule").innerHTML += `<div class="red-border movie-card"><h2>${movieSchedule[i].title}</h2> <h3>${movieSchedule[i].rating}</h3> <h3>Showings today? ${movieSchedule[i].currentlyPlaying}</h3> <img src="${movieSchedule[i].poster}"></div>`;
+  } 
+}
+// You can use any elements you like in your HTML string as long as all the data in the movieSchedule object is included.
+
+// Revisit your movie schedule exercise from above.
+// Add a conditional so that you only print movies where the currentlyPlaying property is equal to true.
+// Use flexbox to arrange your movie schedule side by side on your web page.
+// If a movie does not have a poster property, give it a placeholder image like this
+// Give all G-rated movies a dotted green border that's 2px wide.
+// Give all PG-rated movies a dotted yellow border that's 2px wide.
+// Give all PG-13 movies a dotted orange border that's 2px wide.
+// Give all R-rated movies a dotted red border that's 2px wide.
